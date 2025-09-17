930b45270d22691ab71788843baf7d2512f94cc6
Dmitriy0709 <opt-alko1@mail.ru>
2025-09-17 12:46:40 +0500

    request file upload1

--- mysite/requestdataapp/views.py
+++ mysite/requestdataapp/views.py
@@ -1,3 +1,4 @@
+from django.core.files.storage import FileSystemStorage
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
@@ -12,3 +13,16 @@ def process_get_view(request: HttpRequest) -> HttpResponse:
     }
     return render(request, "requestdataapp/request-query-params.html", context=context)
 
+def user_form(request: HttpRequest) -> HttpResponse:
+    return render(request, "requestdataapp/user-bio-form.html")
+
+def handle_file_upload(request: HttpRequest) -> HttpResponse:
+    saved_file_url = None
+    if request.method == "POST" and request.FILES.get("myfile"):
+        myfile = request.FILES["myfile"]
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        saved_file_url = fs.url(filename)
+        print("saved file", filename)
+
+    return render(request, "requestdataapp/file-upload.html", context={"saved_file_url": saved_file_url})
