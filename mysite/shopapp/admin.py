27f75b74e9aabfe173737c785366497cb81fe6bb
Dmitriy0709 <opt-alko1@mail.ru>
2025-11-06 10:23:31 +0500

    update4

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -1,74 +1,125 @@
 from django.contrib import admin
-from django.db.models import QuerySet
-from django.http import HttpRequest
+from .models import Product, Category, Order, OrderItem
 
-from .models import Product, Order
-from .admin_mixins import ExportAsCSVMixin
-
-class OrderInline(admin.TabularInline):
-    model = Product.orders.through
-
-@admin.action(description="Archived products")
-def mark_archived(modeladmin: admin.ModelAdmin, request: HttpRequest, querysert: QuerySet):
-    querysert.update(archived=True)
-
-
-@admin.action(description="Unarchived products")
-def mark_unarchived(modeladmin: admin.ModelAdmin, request: HttpRequest, querysert: QuerySet):
-    querysert.update(archived=False)
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin, ExportAsCSVMixin):
-    actions = [
-        mark_archived,
-        mark_unarchived,
-        "export_csv",
-    ]
-    inlines = [
-        OrderInline,
-    ]
-    #list_display = "pk", "name", "description", "price", "discount"
-    list_display = "pk", "name", "description_short", "price", "discount", "archived"
-    list_display_links = "pk", "name"
-    ordering = "name", "pk"
-    search_fields = "name", "description"
-    fieldsets = [
-        (None, {
-            "fields": ("name", "description"),
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Админ-интерфейс для модели Product
+    """
+    # Поля, отображаемые в списке товаров
+    list_display = ('name', 'price', 'quantity', 'created_by', 'is_active', 'created_at')
+
+    # Фильтры на странице списка
+    list_filter = ('is_active', 'created_at', 'created_by')
+
+    # Поля для поиска
+    search_fields = ('name', 'description')
+
+    # Поля, которые нельзя редактировать
+    readonly_fields = ('created_at', 'updated_at', 'created_by')
+
+    # Организация полей в группы
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('name', 'description', 'price', 'quantity', 'image')
         }),
-        ("Price options", {
-            "fields": ("price", "discount"),
-            "classes": ("wide", "collapse",),
+        ('Статус', {
+            'fields': ('is_active',)
         }),
-        ("Extra opions",{
-            "fields": ("archived",),
-            "classes": ("collapse",),
-            "description": "Extra options. Field 'archived' is for soft delete",
-        })
-    ]
+        ('Метаданные', {
+            'fields': ('created_by', 'created_at', 'updated_at'),
+            'classes': ('collapse',)  # Свернуть по умолчанию
+        }),
+    )
+
+    def save_model(self, request, obj, form, change):
+        """
+        Переопределение метода сохранения для автоматического
+        установления created_by при создании товара
+        """
+        if not change:  # Если это новый товар (не редактирование)
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+
+    def get_readonly_fields(self, request, obj=None):
+        """
+        Сделать поле created_by только для чтения при редактировании
+        """
+        readonly = list(super().get_readonly_fields(request, obj))
+        if obj:  # Если редактируем существующий объект
+            readonly.append('created_by')
+        return readonly
 
 
-    def description_short(self, obj: Product) -> str:
-        if len(obj.description) <48:
-            return obj.description
-        return obj.description[:48] + "..."
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Админ-интерфейс для модели Category
+    """
+    # Поля, отображаемые в списке
+    list_display = ('name', 'created_at')
 
-#admin.site.register(Product, ProductAdmin)
+    # Поля для поиска
+    search_fields = ('name', 'description')
 
-class ProductInline(admin.TabularInline):
-    model = Order.products.through
+    # Порядок сортировки
+    ordering = ('name',)
+
+
+class OrderItemInline(admin.TabularInline):
+    """
+    Встроенный редактор для модели OrderItem
+    Позволяет редактировать товары в заказе прямо со страницы заказа
+    """
+    model = OrderItem
+    # Количество пустых форм для добавления товаров
+    extra = 1
+    # Поля, отображаемые в таблице
+    fields = ('product', 'quantity', 'price')
+    # Поля, которые нельзя редактировать
+    readonly_fields = ('price',)
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    inlines = [
-        ProductInline,
-    ]
-    list_display = "delivery_address", "promocode", "created_at", "user_verbose"
+    """
+    Админ-интерфейс для модели Order
+    """
+    # Поля, отображаемые в списке заказов
+    list_display = ('id', 'user', 'status', 'total_amount', 'created_at')
 
-    def get_queryset(self, request):
-        return Order.objects.select_related("user").prefetch_related("products")
+    # Фильтры на странице списка
+    list_filter = ('status', 'created_at', 'user')
 
-    def user_verbose(self, obj: Order) -> str:
-        return obj.user.first_name or obj.user.username
+    # Поля для поиска
+    search_fields = ('user__username', 'user__email')
 
+    # Поля, которые нельзя редактировать
+    readonly_fields = ('created_at', 'updated_at', 'user')
+
+    # Организация полей в группы
+    fieldsets = (
+        ('Информация о заказе', {
+            'fields': ('user', 'status')
+        }),
+        ('Сумма', {
+            'fields': ('total_amount',)
+        }),
+        ('Временные метки', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)  # Свернуть по умолчанию
+        }),
+    )
+
+    # Встроенное редактирование товаров в заказе
+    inlines = [OrderItemInline]
+
+    def get_readonly_fields(self, request, obj=None):
+        """
+        Сделать поле user только для чтения при редактировании заказа
+        """
+        readonly = list(super().get_readonly_fields(request, obj))
+        if obj:  # Если редактируем существующий заказ
+            readonly.append('user')
+        return readonly
--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,125 +1,224 @@
-from timeit import default_timer
-
-from django.contrib.auth.models import Group
-from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView,
+    DeleteView, FormView
+)
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
-from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
-from .forms import GroupForm
-from .forms import ProductForm, OrderForm
-from .models import Product, Order
+from django.contrib import messages
+from django.http import HttpResponseForbidden
+from .models import Product, Category, Order, OrderItem
+from .forms import ProductCreateForm, OrderForm, OrderItemForm
 
 
-class ShopIndexView(View):
-    def get(self, request: HttpRequest) -> HttpResponse:
-        products = [
-            ('Laptop', 1999),
-            ('Desktop', 2999),
-            ('Smartphone', 3999),
-        ]
-        context = {
-            "time_running": default_timer(),
-            "products": products,
-        }
-        return render(request, 'shopapp/shop-index.html', context=context)
-
-class GroupListView(View):
-    def get(self, request: HttpRequest) -> HttpResponse:
-        context = {
-            "form": GroupForm(),
-            "groups": Group.objects.prefetch_related('permissions').all(),
-        }
-        return render(request, 'shopapp/groups-list.html', context=context)
-
-    def post(self, request: HttpRequest):
-        form = GroupForm(request.POST)
-        if form.is_valid():
-            form.save()
-
-        return redirect(request.path)
-
-class ProductsListView(ListView):
-    template_name = "shopapp/products-list.html"
-    context_object_name = "products"
-    queryset = Product.objects.filter(archived=False)
-
-
-class ProductDetailsView(DetailView):
-    template_name = 'shopapp/products-details.html'
+class ProductListView(ListView):
+    """
+    Представление для отображения списка всех активных товаров
+    """
     model = Product
-    context_object_name = "product"
+    template_name = 'shopapp/product_list.html'
+    context_object_name = 'products'
+    # Количество товаров на странице
+    paginate_by = 12
+
+    def get_queryset(self):
+        """Получить только активные товары"""
+        return Product.objects.filter(is_active=True).select_related('created_by')
 
 
-class ProductCreateView(UserPassesTestMixin, CreateView):
-    def test_func(self):
-        #return self.request.user.groups.filter(name="secret-group").exists()
-        return self.request.user.is_superuser
-
+class ProductDetailView(DetailView):
+    """
+    Представление для отображения детальной информации о товаре
+    """
     model = Product
-    fields = "name", "price", "description", "discount"
-    success_url = reverse_lazy("shopapp:products_list")
+    template_name = 'shopapp/product_detail.html'
+    context_object_name = 'product'
+
+    def get_queryset(self):
+        """Получить только активные товары"""
+        return Product.objects.filter(is_active=True)
 
 
-class ProductUpdateView(UpdateView):
+class ProductCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    """
+    Представление для создания нового товара
+    Требует:
+    - Аутентификации (LoginRequiredMixin)
+    - Разрешения 'can_create_product' (PermissionRequiredMixin)
+
+    Автоматически присваивает текущего пользователя как автора товара
+    """
     model = Product
-    fields = "name", "price", "description", "discount"
-    template_name_suffix = "_update_form"
-
-    def get_success_url(self):
-        return reverse(
-            "shopapp:product_details",
-            kwargs={"pk": self.object.pk},
-        )
-
-
-class ProductDeleteView(DeleteView):
-    model = Product
-    success_url = reverse_lazy("shopapp:products_list")
+    form_class = ProductCreateForm
+    template_name = 'shopapp/product_form.html'
+    # URL для редиректа неавторизованных пользователей
+    login_url = 'myauth:login'
+    # Требуемое разрешение
+    permission_required = 'shopapp.can_create_product'
 
     def form_valid(self, form):
-        success_url = self.get_success_url()
-        self.object.archived = True
-        self.object.save()
-        return HttpResponseRedirect(success_url)
+        """
+        Обработка успешной отправки формы
+        Переопределяем метод form_valid согласно заданию:
+        - Устанавливаем текущего пользователя как created_by
+        - Используем super() для вызова родительского метода
+        """
+        # Устанавливаем текущего пользователя как автора товара
+        form.instance.created_by = self.request.user
+        # Добавление сообщения об успехе
+        messages.success(self.request, 'Товар успешно создан!')
+        # Вызов родительского метода для сохранения
+        return super().form_valid(form)
 
-class OrdersListView(LoginRequiredMixin, ListView):
-    template_name = "shopapp/order_list.html"
-    context_object_name = "orders"
-    queryset = (
-        Order.objects.select_related("user").prefetch_related("products")
-    )
-
-
-class OrderDetailView(PermissionRequiredMixin, DetailView):
-    permission_required = "shopapp.view_order"
-    template_name = "shopapp/order_detail.html"
-    context_object_name = "order"
-    queryset = (
-        Order.objects.select_related("user").prefetch_related("products")
-    )
-
-
-class OrderCreateView(CreateView):
-    model = Order
-    form_class = OrderForm
-    template_name = "shopapp/order_form.html"
-    success_url = reverse_lazy("shopapp:orders_list")
-
-
-class OrderUpdateView(UpdateView):
-    model = Order
-    form_class = OrderForm
-    template_name = "shopapp/order_update_form.html"
+    def form_invalid(self, form):
+        """
+        Обработка ошибок формы
+        """
+        # Добавление сообщений об ошибках валидации
+        for field, errors in form.errors.items():
+            for error in errors:
+                messages.error(self.request, f'{field}: {error}')
+        return super().form_invalid(form)
 
     def get_success_url(self):
-        return reverse(
-            "shopapp:order_details",
-            kwargs={"pk": self.object.pk},
-        )
+        """Редирект на страницу товара после создания"""
+        return reverse_lazy('product_detail', kwargs={'pk': self.object.pk})
 
-class OrderDeleteView(DeleteView):
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    """
+    Представление для редактирования товара
+    Ограничения доступа:
+    - Суперпользователь может редактировать любой товар
+    - Остальные пользователи могут редактировать только если:
+      1) У них есть разрешение 'can_edit_product'
+      2) Они являются автором товара
+    """
+    model = Product
+    form_class = ProductCreateForm
+    template_name = 'shopapp/product_form.html'
+    login_url = 'myauth:login'
+
+    def get_queryset(self):
+        """
+        Фильтрация товаров в зависимости от прав пользователя
+        Суперпользователь видит все товары
+        Остальные - только свои товары
+        """
+        if self.request.user.is_superuser:
+            return Product.objects.all()
+        return Product.objects.filter(created_by=self.request.user)
+
+    def dispatch(self, request, *args, **kwargs):
+        """
+        Проверка разрешений перед доступом к представлению
+        """
+        product = self.get_object()
+
+        # Если пользователь НЕ суперпользователь
+        if not request.user.is_superuser:
+            # Проверяем наличие разрешения
+            has_permission = request.user.has_perm('shopapp.can_edit_product')
+            # Проверяем является ли пользователь автором
+            is_author = product.created_by == request.user
+
+            # Если нет разрешения И/ИЛИ не является автором
+            if not (has_permission and is_author):
+                messages.error(
+                    request,
+                    'У вас нет разрешения редактировать этот товар.'
+                )
+                return HttpResponseForbidden('Доступ запрещён.')
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        """
+        Обработка успешной отправки формы
+        """
+        messages.success(self.request, 'Товар успешно обновлён!')
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        """Редирект на страницу товара после обновления"""
+        return reverse_lazy('product_detail', kwargs={'pk': self.object.pk})
+
+
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
+    """
+    Представление для удаления товара
+    Только суперпользователь или пользователь с разрешением 'can_delete_product'
+    и являющийся автором товара может удалить его
+    """
+    model = Product
+    template_name = 'shopapp/product_confirm_delete.html'
+    success_url = reverse_lazy('product_list')
+    login_url = 'myauth:login'
+
+    def get_queryset(self):
+        """
+        Фильтрация товаров при удалении
+        """
+        if self.request.user.is_superuser:
+            return Product.objects.all()
+        return Product.objects.filter(created_by=self.request.user)
+
+    def dispatch(self, request, *args, **kwargs):
+        """
+        Проверка разрешений перед удалением
+        """
+        product = self.get_object()
+
+        # Если пользователь НЕ суперпользователь
+        if not request.user.is_superuser:
+            # Проверяем наличие разрешения на удаление
+            has_permission = request.user.has_perm('shopapp.can_delete_product')
+            # Проверяем является ли пользователь автором
+            is_author = product.created_by == request.user
+
+            # Если нет разрешения И/ИЛИ не является автором
+            if not (has_permission and is_author):
+                messages.error(
+                    request,
+                    'У вас нет разрешения удалить этот товар.'
+                )
+                return HttpResponseForbidden('Доступ запрещён.')
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        """
+        Удаление товара и показание сообщения об успехе
+        """
+        messages.success(request, 'Товар успешно удалён!')
+        return super().delete(request, *args, **kwargs)
+
+
+class OrderListView(LoginRequiredMixin, ListView):
+    """
+    Представление для отображения заказов пользователя
+    """
     model = Order
-    template_name = "shopapp/order_confirm_delete.html"
-    success_url = reverse_lazy("shopapp:order_list")
+    template_name = 'shopapp/order_list.html'
+    context_object_name = 'orders'
+    login_url = 'myauth:login'
+
+    def get_queryset(self):
+        """Получить только заказы текущего пользователя"""
+        return Order.objects.filter(user=self.request.user).prefetch_related('items')
+
+
+class OrderDetailView(LoginRequiredMixin, DetailView):
+    """
+    Представление для отображения деталей заказа
+    Пользователи могут видеть только свои заказы
+    """
+    model = Order
+    template_name = 'shopapp/order_detail.html'
+    context_object_name = 'order'
+    login_url = 'myauth:login'
+
+    def get_queryset(self):
+        """Получить только заказы текущего пользователя"""
+        return Order.objects.filter(user=self.request.user)
