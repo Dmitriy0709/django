63b9951c6b553c2a68391e78c71ddf77441b88c4
Dmitriy0709 <opt-alko1@mail.ru>
2025-11-11 14:23:14 +0500

    update setings new2

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shopapp.urls')),
     path('req/', include('requestdataapp.urls')),
     path('accounts/', include('myauth.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
