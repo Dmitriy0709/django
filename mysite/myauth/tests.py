c96d8de0dc86a636c9044ab0857a0703bb5fab09
Dmitriy0709 <opt-alko1@mail.ru>
2025-11-19 15:30:20 +0500

    update setings change tests2

--- mysite/myauth/tests.py
+++ mysite/myauth/tests.py
@@ -1,9 +1,22 @@
+# myauth/tests.py
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class GetCookieViewTestCase(TestCase):
-    def test_get_cookie_view(self):
-        response = self.client.get(reverse("accounts:get_cookie"))
-        self.assertContains(response, "Cookie value")
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
 
+    def test_get_cookie_view(self):
+        self.client.login(username='testuser', password='testpass123')
+
+        response = self.client.get(
+            reverse("accounts:get_cookie"),
+            HTTP_USER_AGENT="Mozilla/5.0 Test"
+        )
+        # Измени на реальный текст из шаблона
+        self.assertContains(response, "Your Cookie Data")
