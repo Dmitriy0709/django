713a26286fed89804a97295a5619b563fc397052
Dmitriy0709 <opt-alko1@mail.ru>
2025-11-19 15:11:15 +0500

    update setings change tests1

--- mysite/myauth/tests.py
+++ mysite/myauth/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class GetCookieViewTestCase(TestCase):
+    def test_get_cookie_view(self):
+        response = self.client.get(reverse("accounts:get_cookie"))
+        self.assertContains(response, "Cookie value")
+
