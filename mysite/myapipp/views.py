2ff9b1797cb0be8a7679f7e51b37c0185dc76510
Dmitriy0709 <opt-alko1@mail.ru>
2025-12-02 14:54:27 +0500

    api 5

--- mysite/myapipp/views.py
+++ mysite/myapipp/views.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import Group
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin
+from rest_framework.generics import GenericAPIView, ListCreateAPIView
+#from rest_framework.mixins import ListModelMixin
 
 from .serializers import GroupSerializer
 
@@ -12,8 +12,6 @@ def hello_world_view(request: Request) -> Response:
     return Response({"message": "Hello world!"})
 
 
-class GroupsListView(ListModelMixin, GenericAPIView):
+class GroupsListView(ListCreateAPIView):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    def get(self, request: Request) -> Response:
-        return self.list(request)
