a1beffe364ebe7f0440eb14b28ced382762deb33
Dmitriy0709 <opt-alko1@mail.ru>
2025-12-02 14:48:52 +0500

    api 3

--- mysite/myapipp/views.py
+++ mysite/myapipp/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.views import APIView
 
+from .serializers import GroupSerializer
 
 @api_view()
 def hello_world_view(request: Request) -> Response:
@@ -13,5 +14,5 @@ def hello_world_view(request: Request) -> Response:
 class GroupsListView(APIView):
     def get(self, request: Request) -> Response:
         groups = Group.objects.all()
-        data = [group.name for group in groups]
-        return Response({"groups": data})
+        serialized = GroupSerializer(groups, many=True)
+        return Response({"groups": serialized.data})
