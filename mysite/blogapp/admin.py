428fbc84a7bffceab3a38820cdf5e610ba317862
Dmitriy0709 <opt-alko1@mail.ru>
2025-12-09 14:39:43 +0500

    blog 1

--- mysite/blogapp/admin.py
+++ mysite/blogapp/admin.py
@@ -1,3 +1,66 @@
+"""
+Админ-панель для приложения blogapp.
+"""
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import Author, Category, Tag, Article
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """
+    Админ-интерфейс для модели Author.
+    """
+    list_display = ['name', 'created_at']
+    search_fields = ['name', 'bio']
+    ordering = ['name']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Админ-интерфейс для модели Category.
+    """
+    list_display = ['name']
+    search_fields = ['name']
+    ordering = ['name']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    """
+    Админ-интерфейс для модели Tag.
+    """
+    list_display = ['name']
+    search_fields = ['name']
+    ordering = ['name']
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    """
+    Админ-интерфейс для модели Article.
+    """
+    list_display = ['title', 'author', 'category', 'pub_date', 'tag_count']
+    list_filter = ['pub_date', 'category', 'author']
+    search_fields = ['title', 'content', 'author__name']
+    readonly_fields = ['pub_date', 'updated_at']
+    fieldsets = (
+        (_('Main Information'), {
+            'fields': ('title', 'author', 'category')
+        }),
+        (_('Content'), {
+            'fields': ('content',)
+        }),
+        (_('Tags and Dates'), {
+            'fields': ('tags', 'pub_date', 'updated_at')
+        }),
+    )
+
+    def tag_count(self, obj):
+        """
+        Отображает количество тегов для статьи.
+        """
+        return obj.tags.count()
+    tag_count.short_description = _('Tags count')
--- mysite/blogapp/views.py
+++ mysite/blogapp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+"""
+Представления для приложения blogapp.
+"""
+from django.views.generic import ListView
+from django.db.models import Prefetch
 
-# Create your views here.
+from .models import Article
+
+
+class ArticleListView(ListView):
+    """
+    Представление для отображения списка всех статей с оптимизацией запросов.
+
+    Используется:
+    - select_related для подгрузки автора и категории (ForeignKey)
+    - prefetch_related для подгрузки тегов (ManyToMany)
+    - defer для исключения поля content из основного запроса
+    """
+    model = Article
+    template_name = 'blogapp/article_list.html'
+    context_object_name = 'articles'
+    paginate_by = 10
+
+    def get_queryset(self):
+        """
+        Оптимизирует запрос к БД для избежания N+1 проблемы.
+        """
+        return Article.objects.select_related(
+            'author',  # Подгрузка автора (ForeignKey)
+            'category',  # Подгрузка категории (ForeignKey)
+        ).prefetch_related(
+            'tags',  # Подгрузка тегов (ManyToMany)
+        ).defer(
+            'content',  # Исключаем большое поле content из запроса
+        ).all()
