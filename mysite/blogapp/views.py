68fa0f3c41fa02cda8664328839d4cd3d12432aa
Dmitriy0709 <opt-alko1@mail.ru>
2025-12-25 15:27:25 +0500

    upload csv23

--- mysite/blogapp/views.py
+++ mysite/blogapp/views.py
@@ -36,26 +36,28 @@ class ArticleListView(ListView):
             'content',  # Исключаем большое поле content из запроса
         ).all()
 
+
 class LatestArticlesFeed(Feed):
-    title = "Blog articles (lates)"
+    title = "Blog articles (latest)"
     description = "Updates on changes and addition blog articles"
-    lonk = reverse_lazy("blogapp:articles")
+
+    def link(self):
+        # ✅ ИСПРАВЛЕНО: Используйте правильное имя URL
+        return reverse("blogapp:article-list")
 
     def items(self):
         return (
             Article.objects.filter(
-                published_at_isnull=False)
-            .order_by("-published_at")[:5]
+                published_at__isnull=False
             )
+            .order_by("-published_at")[:5]
+        )
 
     def item_title(self, item: Article):
         return item.title
 
-    def item_description(self, item:Article):
+    def item_description(self, item: Article):
         return item.body[:200]
 
-    def item_link(self, item:Article):
+    def item_link(self, item: Article):
         return reverse("blogapp:article", kwargs={"pk": item.pk})
-
-
-
