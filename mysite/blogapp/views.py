ea4be8e64c67d327b48e17445e85b604a2034f2f
Dmitriy0709 <opt-alko1@mail.ru>
2025-12-25 15:15:44 +0500

    upload csv20

--- mysite/blogapp/views.py
+++ mysite/blogapp/views.py
@@ -1,8 +1,10 @@
 """
 Представления для приложения blogapp.
 """
+from django.contrib.syndication.views import Feed
 from django.views.generic import ListView
 from django.db.models import Prefetch
+from django.urls import reverse, reverse_lazy
 
 from .models import Article
 
@@ -33,3 +35,27 @@ class ArticleListView(ListView):
         ).defer(
             'content',  # Исключаем большое поле content из запроса
         ).all()
+
+    class LatestArticlesFeed(Feed):
+        title = "Blog articles (lates)"
+        description = "Updates on changes and addition blog articles"
+        lonk = reverse_lazy("blogapp:articles")
+
+        def items(self):
+            return (
+                Article.objects.filter(
+                    published_at_isnull=False)
+                .order_by("-published_at")[:5]
+                )
+
+        def item_title(self, item: Article):
+            return item.title
+
+        def item_description(self, item:Article):
+            return item.body[:200]
+
+        def item_link(self, item:Article):
+            return reverse("blogapp:article", kwargs={"pk": item.pk})
+
+
+
