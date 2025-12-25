0198d21238566a68557ea94a3a78573a939e2ae6
Dmitriy0709 <opt-alko1@mail.ru>
2025-12-25 13:23:41 +0500

    upload csv10

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -1,7 +1,12 @@
+from io import TextIOWrapper
+from csv import DictReader
+
 from django.contrib import admin
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import path
+from werkzeug.utils import redirect
+
 from .models import Product, Order, ProductImage
 from .admin_mixins import ExportAsCSVMixin
 from .forms import CSVImportForm
@@ -24,11 +29,32 @@ class ProductAdmin(admin.ModelAdmin, ExportAsCSVMixin):
     mark_as_archived.short_description = 'Mark selected products as archived'
 
     def import_csv(self, request: HttpRequest) -> HttpResponse:
-        form = CSVImportForm()
-        context = {
-            "form": form,
-        }
-        return render(request, "admin/csv_form.html", context)
+        if request.method == "GET":
+            form = CSVImportForm()
+            context = {
+                "form": form,
+            }
+            return render(request, "admin/csv_form.html", context)
+        form = CSVImportForm(request.POST, request.FILES)
+        if not form.is_valid():
+            context = {
+                "form": form,
+            }
+            return render(request, "admin/csv_form.html", context, status=400)
+
+        csv_file = TextIOWrapper(
+            form.files["csv_file"].file,
+            encoding=request.encoding,
+        )
+        reader = DictReader(csv_file)
+
+        products = [
+            Product(**row)
+            for row in reader
+        ]
+        Product.objects.bulk_create(products)
+        self.message_user(request, "data from CSV was imported")
+        return redirect("..")
 
     def get_urls(self):
         urls = super().get_urls()
