b9ac837816e07cfa48d3fab96b9d1737a49b5910
Dmitriy0709 <opt-alko1@mail.ru>
2025-12-25 15:19:25 +0500

    upload csv22

--- mysite/blogapp/urls.py
+++ mysite/blogapp/urls.py
@@ -3,10 +3,11 @@ URL configuration for blogapp.
 """
 from django.urls import path
 from . import views
+from .views import LatestArticlesFeed
 
 app_name = 'blogapp'
 
 urlpatterns = [
     path('articles/', views.ArticleListView.as_view(), name='article-list'),
-    path('articles/latest/feed', LatestArticlesFeed(), name="articles-feed"),
+    path('articles/latest/feed/', LatestArticlesFeed(), name="articles-feed"),
 ]
