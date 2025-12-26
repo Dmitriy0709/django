d3817cc44f927c9ef6afbd02e165fbda530cfc1c
Dmitriy0709 <opt-alko1@mail.ru>
2025-12-26 10:10:08 +0500

    sitemap2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.saitmaps',
+
 
     # Third-party apps
     'rest_framework',
