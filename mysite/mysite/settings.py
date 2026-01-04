079a4764291d6d4c30236361b9cd5444a690243e
Dmitriy0709 <opt-alko1@mail.ru>
2026-01-04 22:54:17 +0500

    django cache dz 2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -68,7 +68,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.cache.UpdateCacheMiddleware,'
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
