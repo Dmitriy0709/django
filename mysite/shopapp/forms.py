2244c5ef806fa35c4e5b60dc247cb40039e59de6
Dmitriy0709 <opt-alko1@mail.ru>
2026-01-04 20:01:36 +0500

    new site5

--- mysite/shopapp/forms.py
+++ mysite/shopapp/forms.py
@@ -1,4 +1,24 @@
 from django import forms
+from .models import Product
+
+
+class ProductForm(forms.ModelForm):
+    """
+    Форма для создания и редактирования продуктов.
+    """
+    class Meta:
+        model = Product
+        fields = ('name', 'description', 'price')
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'price': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01'}),
+        }
+
+
+class CSVImportForm(forms.Form):
+    """Форма для импорта CSV файлов"""
+    csv_file = forms.FileField()
 
 
 class OrderCSVImportForm(forms.Form):
