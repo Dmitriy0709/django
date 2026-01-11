83098fe3db53bac00cb5d7469562de8f854d67fe
Dmitriy0709 <opt-alko1@mail.ru>
2026-01-11 19:36:00 +0500

    git new

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,6 +28,8 @@ sentry_sdk.init(
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DATABASE_DIR = BASE_DIR / "database"
+DATABASE_DIR.mkdir(exist_ok=True)
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,7 +127,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': DATABASE_DIR / 'db.sqlite3',
         }
     }
 
